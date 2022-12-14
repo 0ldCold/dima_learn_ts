0421eb97457ce50d253760f60d739fc85fe4ddac
Aeronnax <aeronnaxog@gmail.com>
2022-12-14 23:29:17 +0600

    start create api

--- src/anotherCode/api/instance.ts
+++ src/anotherCode/api/instance.ts
@@ -1,5 +1,13 @@
 import axios from "axios";
 
 export const baseUrl = process.env.BASE_URL ?? "http://localhost";
+const nasaApiKey = process.env.NASA_API_KEY;
 
-export const instance = axios.create();
+export const instance = axios.create({ baseURL: baseUrl });
+
+instance.interceptors.request.use((config) => {
+  const previousParams = config.params;
+  config.params = { ...previousParams, api_key: nasaApiKey };
+
+  return config;
+});
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { ButtonClickType } from "src/types";
+import { testApi } from "./api";
 
 export const onButtonClick: ButtonClickType = async (): Promise<void> => {
   console.log("clicked");
+  const response = await testApi();
+  console.log(response);
 };
