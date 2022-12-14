b7d526fe6e51e4edc931954a4188b95e3a456376
Aeronnax <aeronnaxog@gmail.com>
2022-12-14 23:34:02 +0600

    remove comment

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,6 @@
 import { instance } from "./anotherCode/api/instance";
 
 export const testApi = (): Promise<unknown> => {
-  // start_date=2015-09-07&end_date=2015-09-08
   return instance.get("/feed", {
     params: {
       start_date: "2015-09-07",
